a4fe659da9faa17a43e80488cbc89f91eb9f88a6
Jan Andrzejewski <90138353+Jonaszekk@users.noreply.github.com>
2021-09-17 23:22:02 +0200

    Add files via upload

---
+++ dlugosc_imienia_nazwiska.py
@@ -0,0 +1,9 @@
+print("Licznik długości twojego imienia i nazwiska!")
+imie = input("Podaj swoję imię: " )
+nazwisko = input("Podaj swoję nazwisko: ")
+
+
+print("Długość twojego imienia wynosi: ") 
+print(len(imie))
+print("Długość twojego nazwiska wynosi: ")
+print(len(nazwisko))
---
+++ wieksze_od_drugiej.py
@@ -0,0 +1,8 @@
+a = input("Podaj a: ")
+b = input("Podaj b: ")
+
+if a > b: 
+    print("a jest większe od b")
+
+if a < b: 
+    print("b jest większe od a")
